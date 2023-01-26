b41aea80d780df4c2147fb575207adef3ac3b501
yeuri <yeuri@YEURI>
2023-01-26 11:31:54 -0400

    Injection de dependencias con Hilt

---
+++ app/src/main/java/edu/ucne/tepresto/data/local/dao/OcupacionDao.kt
@@ -0,0 +1,31 @@
+package edu.ucne.tepresto.data.local.dao
+
+import androidx.room.*
+import edu.ucne.tepresto.data.local.entity.OcupacionEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface OcupacionDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(OcupacionEntity: OcupacionEntity)
+
+    @Delete
+    suspend fun delete(OcupacionEntity: OcupacionEntity)
+
+    @Query("""
+        SELECT * 
+        FROM Ocupaciones
+        WHERE OcupacionId=:ocupacionId
+        LIMIT 1
+    """)
+    suspend fun find(ocupacionId: Int): OcupacionEntity?
+
+    @Query("SELECT * FROM Ocupaciones")
+    fun getList(): Flow<List<OcupacionEntity>>
+}
+
+class dao{
+    fun save():Boolean{
+        return true
+    }
+}
